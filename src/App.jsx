5efa51b0bf88b504e97fbe56880f610b50bd059e
Chamara-Wijepala <mr.worf777@gmail.com>
2024-02-27 15:35:05 +0530

    Complete chapter 19

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,74 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { format } from 'date-fns';
+import api from './api/posts';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import NewPost from './pages/NewPost';
 import PostPage from './pages/PostPage';
+import EditPost from './pages/EditPost';
 import About from './pages/About';
 import Missing from './pages/Missing';
 
 function App() {
-	const [posts, setPosts] = useState([
-		{
-			id: 1,
-			title: 'My First Post',
-			datetime: 'July 01, 2021 11:17:36 AM',
-			body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-		},
-		{
-			id: 2,
-			title: 'My 2nd Post',
-			datetime: 'July 01, 2021 11:17:36 AM',
-			body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-		},
-		{
-			id: 3,
-			title: 'My 3rd Post',
-			datetime: 'July 01, 2021 11:17:36 AM',
-			body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-		},
-		{
-			id: 4,
-			title: 'My Fourth Post',
-			datetime: 'July 01, 2021 11:17:36 AM',
-			body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-		},
-	]);
+	const [posts, setPosts] = useState([]);
 	const [search, setSearch] = useState('');
 	const [searchResults, setSearchResults] = useState([]);
 	const [postTitle, setPostTitle] = useState('');
 	const [postBody, setPostBody] = useState('');
+	const [editTitle, setEditTitle] = useState('');
+	const [editBody, setEditBody] = useState('');
 	const navigate = useNavigate();
 
-	function handleDelete(id) {
-		const postsList = posts.filter((post) => post.id !== id);
-		setPosts(postsList);
-		navigate('/');
+	async function handleDelete(id) {
+		try {
+			await api.delete(`/posts/${id}`);
+
+			const postsList = posts.filter((post) => post.id !== id);
+			setPosts(postsList);
+			navigate('/');
+		} catch (error) {
+			console.log(`Error: ${error.message}`);
+		}
 	}
 
-	function handleSubmit(e) {
+	async function handleSubmit(e) {
 		e.preventDefault();
 
 		const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
 		const datetime = format(new Date(), 'MMMM dd, yyyy pp');
 		const newPost = { id, title: postTitle, datetime, body: postBody };
-		const allPosts = [...posts, newPost];
 
-		setPosts(allPosts);
-		setPostTitle('');
-		setPostBody('');
-		navigate('/');
+		try {
+			const response = await api.post('/posts', newPost);
+			const allPosts = [...posts, response.data];
+
+			setPosts(allPosts);
+			setPostTitle('');
+			setPostBody('');
+			navigate('/');
+		} catch (error) {
+			console.log(`Error: ${error.message}`);
+		}
+	}
+
+	async function handleEdit(id) {
+		const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+		const updatedPost = { id, title: editTitle, datetime, body: editBody };
+
+		try {
+			const response = await api.put(`/posts/${id}`, updatedPost);
+			setPosts(
+				posts.map((post) => (post.id === id ? { ...response.data } : post))
+			);
+			setEditTitle('');
+			setEditBody('');
+			navigate('/');
+		} catch (error) {
+			console.log(`Error: ${error.message}`);
+		}
 	}
 
 	useEffect(() => {
@@ -72,6 +80,25 @@ function App() {
 
 		setSearchResults(filteredResults.reverse());
 	}, [posts, search]);
+
+	useEffect(() => {
+		async function fetchPosts() {
+			try {
+				const response = await api.get('/posts');
+				setPosts(response.data);
+			} catch (error) {
+				if (error.response) {
+					console.log(error.response.data);
+					console.log(error.response.status);
+					console.log(error.response.headers);
+				} else {
+					console.log(`Error: ${error.message}`);
+				}
+			}
+		}
+
+		fetchPosts();
+	}, []);
 
 	return (
 		<div className="App">
@@ -89,6 +116,19 @@ function App() {
 							postBody={postBody}
 							setPostBody={setPostBody}
 							handleSubmit={handleSubmit}
+						/>
+					}
+				/>
+				<Route
+					path="/edit/:id"
+					element={
+						<EditPost
+							posts={posts}
+							editTitle={editTitle}
+							setEditTitle={setEditTitle}
+							editBody={editBody}
+							setEditBody={setEditBody}
+							handleEdit={handleEdit}
 						/>
 					}
 				/>
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -13,7 +13,15 @@ function PostPage({ posts, handleDelete }) {
 						<p className="postDate">{post.datetime}</p>
 						<p className="postBody">{post.body}</p>
 
-						<button onClick={() => handleDelete(post.id)}>Delete Post</button>
+						<Link to={`/edit/${post.id}`}>
+							<button className="editButton">Edit Post</button>
+						</Link>
+						<button
+							className="deleteButton"
+							onClick={() => handleDelete(post.id)}
+						>
+							Delete Post
+						</button>
 					</>
 				)}
 				{!post && (
